71045f639edb5b2cd7388ffb3d050f2a7c6e4dd9
senthil kumar <senthil.r@tridentnets.in>
2018-09-22 20:04:29 +0530

    First commit

---
+++ app/src/main/java/com/example/senthil/kotlin_tablayout/Fragment/ContactUsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.senthil.kotlin_tablayout.Fragment
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.senthil.kotlin_tablayout.R
+
+class ContactUsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_us, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/senthil/kotlin_tablayout/TabLayoutActivity.kt
@@ -0,0 +1,54 @@
+package com.example.senthil.kotlin_tablayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import com.example.senthil.kotlin_tablayout.Fragment.AboutUsFragment
+import com.example.senthil.kotlin_tablayout.Fragment.ContactUsFragment
+import com.example.senthil.kotlin_tablayout.Fragment.HomeFragment
+import kotlinx.android.synthetic.main.activity_tab_layout.*
+
+class TabLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_layout)
+
+        setupViewPager(viewpager)
+
+        tabs!!.setupWithViewPager(viewpager)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(AboutUsFragment(), "About Us")
+        adapter.addFragment(ContactUsFragment(), "Contact Us")
+        viewPager.adapter = adapter
+    }
+
+    internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList[position]
+        }
+    }
+}
